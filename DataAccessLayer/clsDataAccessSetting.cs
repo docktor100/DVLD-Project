6d87946b49dff549807226aa94d5655ee3beda45
docktor100 <kingabdullah405@gmail.com>
2025-07-21 22:38:32 +0300

    C20 - Implemented App.config ConnectionString for DB Connection

--- DataAccessLayer/clsDataAccessSetting.cs
+++ DataAccessLayer/clsDataAccessSetting.cs
@@ -1,8 +1,11 @@
-﻿namespace DataAccessLayer
+﻿using System.Configuration;
+
+namespace DataAccessLayer
 {
     static class clsDataAccessSetting
     {
-        public static string ConnectionString = "Server=.;Database=DVLD;User Id=sa; Password=sa123456";
+        //public static string ConnectionString = "Server=.;Database=DVLD;User Id=sa; Password=sa123456"; the old method
 
+        public static string ConnectionString = ConfigurationManager.AppSettings["ConnectionString"];
     }
 }
